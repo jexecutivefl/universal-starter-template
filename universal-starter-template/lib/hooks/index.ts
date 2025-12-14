966ddfb90c54190427f9858586fd1c01283c1e24
jexecutivefl <j.executivefl@gmail.com>
2025-12-14 08:14:46 -0500

    Delete universal-starter-template directory

--- universal-starter-template/lib/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Custom React Hooks
- *
- * Re-export all custom hooks from a single entry point.
- */
-
-export * from './use-debounce';
-export * from './use-local-storage';
-export * from './use-media-query';
